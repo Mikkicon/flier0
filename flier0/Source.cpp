5895861e6bfb5c9ab1c5ca0209d6fdca0ff5a342
Mikkicon <mikkicon@gmail.com>
2018-11-20 09:44:46 +0200

    TAVProperty constructor and getter

---
+++ flier0/Source.cpp
@@ -0,0 +1,9 @@
+#include "pch.h"
+#include <iostream>
+#include "TAVProperty.h"
+
+int main1() {
+	std::cout << "Hello from flier!" << std::endl;
+	//TAVProperty t("name=xyu");
+	return 0;
+}
--- flier0/TAVProperty.h
+++ flier0/TAVProperty.h
@@ -8,19 +8,21 @@ class TAVProperty : AVProperty
 {
 private:
 	T magic_number_1 = 0;
-
+	std::string _name = "";
 public:
 	TAVProperty(T x);
+	TAVProperty(std::string & const);
 	virtual ~TAVProperty();
 	TAVProperty<T>& operator<<(const T&);
-
+	const std::string & getName()const;
 };
 
 template<typename T>
 TAVProperty<T>::TAVProperty(T x) {
 	T y = magic_number_1;
 }
-
+template<typename T>
+TAVProperty<T>::TAVProperty(std::string & const s);
 
 
 #endif //FLIER0_TAVPROPERTY_H
